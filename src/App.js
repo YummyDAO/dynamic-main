366a1b40477a1185c49fe1a582f74503dc6a7789
fantazyone <contact@fantazy.one>
2023-10-07 06:46:16 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,178 @@
+import logo from './logo1.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="page__load"><div className="load-header__wrp">
+    <section className="learn wf-section">
+      <div className="learn__wrp-text">
+        <div className="learn-text">Liquity is LIVE on Ethereum mainnet!</div>
+        <div className="learn-inner">
+          <a href="https://medium.com/liquity/liquity-goes-live-on-ethereum-mainnet-203d295d2fc6" target="_blank" className="learn-link">Learn more</a>
+          <img src="https://uploads-ssl.webflow.com/5fd883457ba5da4c3822b02c/5fe243a0820e833eb80d0a71_Vector%2018.svg" loading="lazy" alt="ico" className="learn-img" />
+        </div>
+      </div>
+    </section>
+    <header className="header wf-section">
+      <div data-w-id="9e93303a-74eb-19ea-e07b-1baa42222fc3" className="top-bar-wrapper---brix none1" style={{display: 'flex'}}><div className="top-bar-container---brix top-bar-2---brix preview-page---brix" style={{opacity: 1}}>
+          <div className="div-block-29"><div className="top-bar-text---brix">
+              <a href="https://swap.puffydao.com/#/swap">Countdown for Realp farming on Puffyswap</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container-__nav w-container">
+        <div className="header__wrp"><div data-collapse="medium" data-animation="default" data-duration={400} data-w-id="c227a731-c018-c755-1dc9-bfb186b8a461" data-easing="ease" data-easing2="ease" role="banner" className="header__navbar w-nav">
+            <div className="header__container">
+              <a href="/" aria-current="page" className="logo__link w-nav-brand w--current" aria-label="home">
+                <img src={logo} loading="lazy" width={250} alt="" className="logo__img" />
+              </a>
+              <nav role="navigation" className="header__navmenu w-nav-menu">
+                <div className="header-navlink__wrp _2">
+                  <a href="https://igurufinance.gitbook.io/iguru-finance-base/" target="_blank" className="header__navlink w-nav-link">Docs</a>
+                </div>
+                <div className="header-navlink__wrp none1">
+                  <a href="https://lend.puffydao.com/#/" className="header__navlink w-nav-link">Frontends</a>
+                </div>
+                {/*<div class="header-navlink__wrp">
+                  <div data-hover="true" data-delay="20" class="w-dropdown">
+                      <div class="dropdown-toggle w-dropdown-toggle" id="w-dropdown-toggle-0" aria-controls="w-dropdown-list-0" aria-haspopup="menu" aria-expanded="false" role="button" tabindex="0">
+                          <div class="text-block-4">Frontends</div></div><nav class="dropdown-list w-dropdown-list" id="w-dropdown-list-0" aria-labelledby="w-dropdown-toggle-0">
+                              <a href="/frontend" class="dropdown-link w-dropdown-link" tabindex="0">Use Liquity</a><a href="/frontend-operators" class="dropdown-link-2 w-dropdown-link" tabindex="0">Run a Frontend</a>
+                          </nav>
+                      </div>
+                  </div>
+                  <div class="header-navlink__wrp">
+                      <div data-hover="true" data-delay="20" class="w-dropdown">
+                          <div class="dropdown-toggle w-dropdown-toggle" id="w-dropdown-toggle-1" aria-controls="w-dropdown-list-1" aria-haspopup="menu" aria-expanded="false" role="button" tabindex="0"><div class="text-block-4">More</div>
+                      </div>
+                      <nav class="dropdown-list w-dropdown-list" id="w-dropdown-list-1" aria-labelledby="w-dropdown-toggle-1">
+                          <a href="/blog" class="dropdown-link w-dropdown-link" tabindex="0">Blog</a>
+                          <a href="https://dune.xyz/dani/Liquity" target="_blank" class="dropdown-link-2 w-dropdown-link" tabindex="0">Analytics</a>
+                          <a href="/team" class="dropdown-link-2 w-dropdown-link" tabindex="0">About Us</a>
+                      </nav>
+                  </div>
+              </div>*/}
+                {/*<div class="header__form-obile-wrp">
+                  <div class="header__form-wrp _2">
+                      <div class="header__forms w-form">
+                          <form id="email-form" name="email-form" data-name="Email Form" method="get" class="header__form" aria-label="Email Form">
+                              <input type="email" class="header__text-field w-input" maxlength="256" name="Email-2" data-name="Email 2" placeholder="Stay up to date" id="Email-2" required="">
+                              <input type="submit" value="Sign up" data-wait="Please wait..." class="header__submit-button w-button"></form><div class="w-form-done" tabindex="-1" role="region" aria-label="Email Form success">
+                                  <div>Thank you! Your submission has been received!</div></div><div class="error-message w-form-fail" tabindex="-1" role="region" aria-label="Email Form failure">
+                                      <div>Oops! Something went wrong while submitting the form.</div>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>*/}
+                {/*Add socials*/}
+              </nav>
+              {/* <div className="header__form-wrp"><div className="header__forms w-form">
+                  <form id="" name="email-form" data-name="Email Form" action="" method="post" className="header__form" aria-label="Email Form">
+                    <input type="email" className="header__text-field w-input" maxLength={256} name="EMAIL" data-name="EMAIL" placeholder="Stay up to date" id="EMAIL-HEADER-FROM" required />
+                    <input type="submit" data-wait="Please wait..." defaultValue="Sign up" className="header__submit-button w-button disabled" />
+                  </form><div className="success-message w-form-done" tabIndex={-1} role="region" aria-label="Email Form success">
+                    <div className="text-block">Thank you! Your submission has been received!</div>
+                  </div>
+                  <div className="error-message w-form-fail" tabIndex={-1} role="region" aria-label="Email Form failure">
+                    <div>Oops! Something went wrong while submitting the form. Please try to use <a href="" target="_blank">this form</a> to sign up.</div>
+                  </div>
+                    </div>
+                    </div>*/}
+              <div className="header__menu-button w-nav-button" style={{WebkitUserSelect: 'text'}} aria-label="menu" role="button" tabIndex={0} aria-controls="w-nav-overlay-0" aria-haspopup="menu" aria-expanded="false">
+                <div className="w-icon-nav-menu" /><img src="https://uploads-ssl.webflow.com/5fd883457ba5da4c3822b02c/5fdf7f1814b4b981bfa634bf_Menu%20close%20item.svg" loading="lazy" alt="menu-ico" className="menu-close__img" style={{opacity: 0}} />
+              </div>
+            </div>
+            <div className="w-nav-overlay" data-wf-ignore id="w-nav-overlay-0">
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  </div>
+  <section className="home-top wf-section">
+    <div className="container landscap-container w-container">
+      <div className="home-top__wrp">
+        <div id="w-node-_2a5b4f1e-2cef-c69d-19ee-d14c2201dfec-148d2055" className="home-top__col"><h1 className="home-top-title__txt"> 
+        The end game for liquid staking tokens
+        <span className="text-span-4"></span>
+        </h1><h2 className="home-top__txt">A non-custodial and decentralized BASE LST-backed stablecoin</h2><div className="home-button__wrp">
+            <a href="" className="home-top__button-trye w-button">Coming Soon</a>
+            <a href="https://igurufinance.gitbook.io/iguru-finance-base/" className="home-top__button-operator w-button">Learn More</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <div className="home-tvl wf-section none1">
+    <div className="container tvl-container w-container">
+      <div className="tvl__val-wrapper">
+        <div className="tvl__val-descr">Total Value Locked</div>
+        <h3 className="tvl__val-usd-value">$ 66700</h3>
+      </div>
+    </div>
+  </div>
+  <section className="home-items-new wf-section">
+    <div className="container-2 w-container">
+      <div className="home-items__wrp-2">
+        <div className="text-wrapper">
+          <div className="div-block-7">
+            <div className="color-box-2 small">
+            </div><img src="https://uploads-ssl.webflow.com/5fd883457ba5da4c3822b02c/61194a7e5ca1aae5e9a6d06e_collateral-ratio-icon.png" loading="lazy" width={25} alt="" className="feature-img" />
+            <h2 className="heading-4">Mint</h2>
+            <p className="bold-text"><strong className="bold-text">Choose your favorite LST and deposit it as collateral and mint your desired amount of igUSD.</strong>
+            </p>
+          </div>
+        </div>
+        <div id="w-node-f6cfd765-8375-5e59-ae4f-c19888f1fc21-148d2055" className="text-wrapper">
+          <div className="div-block-13">
+            <img src="https://uploads-ssl.webflow.com/5fd883457ba5da4c3822b02c/61194a86f792d739392e7615_percentage-icon.png" loading="lazy" width={25} alt="" className="feature-img" />
+            <h2 className="heading-4">Earn</h2><div className="div-block-8 small2">
+            </div>
+            <p className="bold-text">
+              <strong className="bold-text-3">Take advantage of igUSD’s yield opportunities via Curve and Convex pools or  IGURU’s own Stability Pool&nbsp;</strong>
+              <a href="https://real-protocol.gitbook.io/real-protocol-docs/faq/borrowing#what-is-the-minimum-collateral-ratio-mcr-and-the-recommended-collateral-ratio" target="_blank" className='none1'>
+                <span className="text-span-2">
+                  <strong className="bold-text-3">Under normal operation</strong>
+                </span>
+              </a>
+              <strong className="bold-text-3">.&nbsp;</strong>
+            </p><p>
+            </p></div>
+        </div>
+        <div className="text-wrapper">
+          <div className="div-block-14">
+            <div className="div-block-9 small3">
+            </div>
+            <img src="https://uploads-ssl.webflow.com/5fd883457ba5da4c3822b02c/61194a8ec6f1ca4d844628c6_whitelusd-icon.png" loading="lazy" width={25} alt="" className="feature-img" />
+            <h2 className="heading-4">Vote</h2>
+            <p className="bold-text">
+              <strong className="bold-text-2">Earn IGURU for minting igUSD, maintaining an active loan or depositing into the stability pool. Lock this IGURU to direct emissions and take advantage of additional incentives.</strong>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <div className="youtube none1"><div className="div-block-28">
+      <div className="boy">FORKED FROM LIQUITY PROTOCOL</div>
+      <h4 className="with">This an example video from liquity</h4>
+      <div style={{paddingTop: '56.17021276595745%'}} className="w-embed-youtubevideo youtube">
+        <iframe src="https://www.youtube-nocookie.com/embed/RcnMe7ScnNA?rel=0&controls=1&autoplay=0&mute=0&start=0" frameBorder={0} style={{position: 'absolute', left: 0, top: 0, width: '100%', height: '100%', pointerEvents: 'auto'}} allow="autoplay; encrypted-media" allowFullScreen title="What is Liquity?">
+        </iframe>
+      </div>
+    </div>
+    <section className="faq wf-section">
+      <div className="container faq-cont w-container">
+        <div className="faq-button__wrp">
+          <a href="https://lend.puffydao.com/#/" target="_blank" className="faq-button faq-cta w-button">Borrow Now</a>
+          <a href="https://real-protocol.gitbook.io/real-protocol-docs/faq/general" target="_blank" className="faq-button w-button">Learn More</a>
+        </div>
+      </div>
+    </section>
+  </div>
+</div>
+  );
+}
+
+export default App;
